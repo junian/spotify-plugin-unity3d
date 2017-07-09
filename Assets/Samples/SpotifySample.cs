f31649e3e08612496d330b22a99634bd57e57a1e
Junian <git@junian.net>
2017-07-09 08:35:03 +0700

    added sampel, spotify for macOS works

---
+++ Assets/Samples/SpotifySample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Net.Junian.UniPlugins;
+using UnityEngine;
+
+public class SpotifySample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayMusic()
+    {
+        SpotifyPlayer.Instance.Play("spotify:track:5uJgz2iuvfePb9RFJYy4pu");
+    }
+}
